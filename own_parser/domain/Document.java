78d0234da9e2f049573aaf84f05ae6a7e3c60026
Vladislav <kostevichvladislav@gmail.com>
2016-06-06 22:29:13 +0300

    Удалены лишние файлы

--- own_parser/domain/Document.java
+++
@@ -1,8 +0,0 @@
-package by.epam.parsers.own_parser.domain;
-
-/**
- * Created by Владислав on 21.05.2016.
- */
-public interface Document extends Node {
-    Element getDocumentElement();
-}
--- own_parser/domain/Text.java
+++
@@ -1,9 +0,0 @@
-package by.epam.parsers.own_parser.domain;
-
-/**
- * Created by Владислав on 21.05.2016.
- */
-public interface Text extends Node {
-    String getWholeText();
-    void replaceWholeText(String content);
-}
